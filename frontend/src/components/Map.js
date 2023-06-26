5b2d4c6f1768b7d6794e0bbfb6383d45aaefa9b5
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-06-26 20:45:23 +0100

    Made changes for frontend/backend/ml communication

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -417,7 +417,7 @@ function Map() {
   }
   
   useEffect(() => {
-    fetch('output.json') // this our backend
+    fetch('http://127.0.0.1:5000/api/predict/teststring') // this our backend
     .then(response => {
       if (!response.ok) { throw new Error('Network response was not ok'); }
       return response.json();
--- machine learning/controllers/prediction.py
+++ machine learning/controllers/prediction.py
@@ -3,6 +3,6 @@ import json
 def prediction(datetime):
     #do some work with metrics, return the prediction
     print("datetime:", datetime)
-    f = open("static/MOCK_DATA.json") #may have to change path after migration to server
+    f = open("static/output.json") #may have to change path after migration to server
     data = json.load(f)
     return data 
