1d071874a3e9c90963c8aab786675f1f8679f5f1
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-06-26 10:54:17 +0100

    Final Update Week 3

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ const port = process.env.PORT || 5000;
 //Middleware:
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", function(req, res) {res.sendFile(path.join(__dirname, "build", "index.html"))})
+//app.use(express.static(path.join(__dirname, "build")));
+//app.get("/", function(req, res) {res.sendFile(path.join(__dirname, "build", "index.html"))})
 
 //Routes:
 app.use("/api/meta", meta)
