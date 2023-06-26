1af4b8fe4473a02d1ac91769a67fd5426d6b3844
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-06-26 11:55:48 +0100

    Added comment to route in app.py

--- machine learning/app.py
+++ machine learning/app.py
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 def predict(datetime):
     #do some work with metrics, return the prediction
     print("datetime:", datetime)
-    f = open("static/MOCK_DATA.json")
+    f = open("static/MOCK_DATA.json") #may have to change path after migration to server
     data = json.load(f)
     return data 
 
