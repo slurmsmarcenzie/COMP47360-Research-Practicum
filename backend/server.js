a186e843ce124f6192916d8a03f7c7e14ae34516
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-06-28 16:02:31 +0100

    Imported logger and httpLogger

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ const app = express()
 const cors = require("cors");
 const meta = require("./routes/meta")
 const prediction = require("./routes/prediction")
+const port = process.env.PORT || 5000
+const {logger, resLogger} = require("../logging/backend/express/logger")
 require("dotenv").config();
-const port = process.env.PORT || 5000;
-
 
 //Middleware:
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 //Below is used to serve a build version of the React frontend application:
 //  app.use(express.static(path.join(__dirname, "build")));
 //  app.get("/", function(req, res) {res.sendFile(path.join(__dirname, "build", "index.html"))})
@@ -18,12 +18,15 @@ app.use(express.json());
 app.use("/api/meta", meta)
 app.use("/api/predict", prediction)
 
+
 app.get('/', (req, res) => {
   res.status(200).send("Home Page. I will serve React App later :)")
+  logger.info("test of logger")
+  resLogger.info("request made for '/'")
 });
 
 app.get('*', (req, res) => {
-  res.status(404).send("Unknown route. Please check the URL entered");
+  res.status(404).send("Unknown route. Please check the URL entered")
 });
 
 //START SERVER
