7f5e8769be5adbde23c5a9c172e791a1641e68a6
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-06-29 19:36:40 +0100

    Added generalLogger to controller

--- backend/controllers/prediction.js
+++ backend/controllers/prediction.js
@@ -1,21 +1,20 @@
-const axios = require("axios")
+const axios = require("axios");
+const { generalLogger } = require("../../logging/backend/express/logger");
 
 //fetch prediction from ML:
-const queryPrediction = (req, res) => {
+const queryPrediction = (req, res, next) => {
     // get date from params and fix it to be the correct format
     let date = new Date(Date.parse(req.params.date)).toISOString();
-    // TODO - add to log
-    console.log("prediction requested for:", req.params)
-    console.log("converted to ISOString: ", date);
+    generalLogger.info(`prediction requested for: ${req.params}`)
+    generalLogger.info(`converted to ISOString: ${date}`);
 
     const uri = `http://127.0.0.1:7000/predict/${date}` 
 
     axios.get(uri)
       .then(response => {
         if (response.data === null || response.data === undefined || response.data.length === 0){
-          res.status(200).json([]); //send empty JSON list if bad response received
-          //TODO - add to log
-          console.log("warning, prediction list is empty")
+          generalLogger.warn(`prediction list is empty: ${response.data}`)
+          res.status(200).json([]); //send empty JSON list if empty response received
         }
         else {
           //make sure response has correct format [location_id, busyness_score]:
@@ -25,10 +24,12 @@ const queryPrediction = (req, res) => {
                 data.push(item);
               }
               else {
-                console.log("warning: prediction item skipped (incorrect format)");
+                generalLogger.warn(`prediction item skipped (incorrect format): ${item}`);
               }
             }
+            generalLogger.info("prediction result OK")
             res.status(200).json(data) //response is OK and contains data
+            
         }
       })
       .catch(error => {
