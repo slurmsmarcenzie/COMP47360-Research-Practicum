1f6dea9ff07bb3f4f6454ae512d233ac9af0e259
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-03 18:54:35 +0100

    Removed console.log statement

--- backend/middleware/http_logger.js
+++ backend/middleware/http_logger.js
@@ -2,7 +2,6 @@ const {httpLogger} = require("../../logging/backend/express/logger")
 
 const http_logger = (req, res, next) => {
     if (res.statusCode < 400){
-        console.log("hello C")
     } else {
         httpLogger.error(res);
     }
