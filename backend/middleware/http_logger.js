da66fc1befe1edf898b8bbf1359a795e3ed37567
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-04 12:43:50 +0100

    Removed next arg

--- backend/middleware/http_logger.js
+++ backend/middleware/http_logger.js
@@ -1,6 +1,6 @@
 const httpLogger = require("../logging/httpLogger")
 
-const http_logger = (req, res, next) => {
+const http_logger = (req, res) => {
     if (res.statusCode < 400){
         httpLogger.info(res);
     } else {
