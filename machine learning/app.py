580c3c1af2b580d0e966cf5d46eafccdc3eacf94
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-12 13:38:43 +0100

    Improved Security

--- machine learning/app.py
+++ machine learning/app.py
@@ -17,11 +17,21 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLDB")
 app.config["SECRET_KEY"] = os.getenv("SECRETKEY")
 app.config["RECAPTCHA_PUBLIC_KEY"] = os.getenv("RECAPTCHA_PUBLIC")
 app.config["RECAPTCHA_PRIVATE_KEY"] = os.getenv("RECAPTCHA_PRIVATE")
+app.config["SESSION_COOKIE_SAMESITE"] = "strict"
+app.config["SESSION_COOKIE_HTTPONLY"] = True
 db.init_app(app)
 app.register_blueprint(info)
 app.register_blueprint(predict)
 app.register_blueprint(base)
 login_manager.init_app(app)
+
+@app.after_request
+def set_headers(response):
+    ##Note: these may need to be set at the web server level, not here
+    response.headers['Content-Security-Policy'] = "default-src 'self'"
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    return response
 
 
 ## After Request decorator is used to log every http request/response in the app.
