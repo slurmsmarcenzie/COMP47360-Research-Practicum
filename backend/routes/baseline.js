b121c7fc13ad2f5b02ed2b56f9d3880969d57535
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-13 17:56:54 +0100

    Updated prediction route. Added event baseline

--- backend/routes/baseline.js
+++ backend/routes/baseline.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const {queryBaseline} = require("../controllers/baseline");
 
 router.route("/:date").get(queryBaseline);
+router.route("/:date/:event")
 
 module.exports = router;
