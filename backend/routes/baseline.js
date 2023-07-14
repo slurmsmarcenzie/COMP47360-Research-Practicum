75bd11ed0c7cdc4703127c670a7ea115947bab5d
harryocleirigh <91558668+harryocleirigh@users.noreply.github.com>
2023-07-14 15:22:59 +0100

    adding changes to backend which allowed for simulataneous fetch requests to be made

--- backend/routes/baseline.js
+++ backend/routes/baseline.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {queryBaseline} = require("../controllers/baseline");
+const {queryBaseline, queryEventBaseline} = require("../controllers/baseline");
 
 router.route("/:date").get(queryBaseline);
-router.route("/:date/:event")
+router.route("/:date/:event").get(queryEventBaseline)
 
 module.exports = router;
--- machine learning/controllers/prediction.py
+++ machine learning/controllers/prediction.py
@@ -40,7 +40,7 @@ def prediction(date, event):
         extracted = []
 
         for item in original:
-            if item["Event_ID"] == eventID and item["time"] == 12:
+            if item["Event_ID"] == eventID and item["time"] == 18:
                 extracted.append(item)
         
         outputjson = json.dumps(extracted)
