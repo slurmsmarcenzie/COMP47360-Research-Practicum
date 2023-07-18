9c7443a92b2a189e7813d61049ac336e1c18abc1
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-18 17:59:13 +0100

    Use build folder

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require("cors");
 const meta = require("./routes/meta")
 const prediction = require("./routes/prediction")
+const path = require("path")
 const baseline = require("./routes/baseline")
 const port = process.env.PORT || 5000
 const http_logger = require("./middleware/http_logger");
@@ -15,23 +16,23 @@ require("dotenv").config();
 //Middleware:
 app.use(cors());
 app.use(express.json())
-app.use(helmet.frameguard())
-app.use(helmet.noSniff());
-app.use(helmet.contentSecurityPolicy())
+// app.use(helmet.frameguard())
+// app.use(helmet.noSniff());
+//app.use(helmet.contentSecurityPolicy())
 app.use(rateLimiter)
 //Below is used to serve a build version of the React frontend application:
-//  app.use(express.static(path.join(__dirname, "build")));
-//  app.get("/", function(req, res) {res.sendFile(path.join(__dirname, "build", "index.html"))})
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", function(req, res) {res.sendFile(path.join(__dirname, "build", "index.html"))})
 
 //Routes:
 app.use("/api/meta", meta, http_logger)
 app.use("/api/predict", prediction, http_logger)
 app.use("/api/baseline", baseline, http_logger)
 
-app.get('/', (req, res, next) => {
-  res.status(200).send("Home Page. I will serve React App later :)")
-  next()
-}, http_logger);
+// app.get('/', (req, res, next) => {
+//   res.status(200).send("Home Page. I will serve React App later :)")
+//   next()
+// }, http_logger);
 
 app.get('*', (req, res, next) => {
   res.status(404).send("Unknown route. Please check the URL entered")
