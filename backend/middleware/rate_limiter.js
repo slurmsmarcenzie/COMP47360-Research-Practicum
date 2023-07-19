d7cbb2c78f308fc09a26c0da222122cb2a98680a
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-19 11:13:44 +0100

    Update rate limiter to allow more requests

--- backend/middleware/rate_limiter.js
+++ backend/middleware/rate_limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit")
 
 const rateLimiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 min
-	max: 10, // Limit to 10 requests per minute
+	max: 30, // Limit to 10 requests per minute
 	standardHeaders: true,
 	legacyHeaders: false
 })
