7575a2df6047310bb3c4c9b27c7475ad5321fe21
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-20 16:16:35 +0100

    Implemented normalisation of model results Now use xgb_non_normalised_merged_model

--- machine learning/predicting/current_busyness_prediction/predict.py
+++ machine learning/predicting/current_busyness_prediction/predict.py
@@ -7,19 +7,22 @@ location_ids = [236, 42, 166, 68, 163, 87, 152, 141, 229, 90, 113, 79, 140, 151,
 
 
 def general_prediction(date):
-    pickled_model = pickle.load(open('predicting/models/equal_weighted_merged_model.pkl', 'rb'))
+    pickled_model = pickle.load(open('predicting/models/xgb_non_normalised_merged_model.pkl', 'rb'))
     data = []
     input_data = generate_model_input(date)
 
     for loc in location_ids:
-        input_data[0] = loc
-        df = pd.DataFrame([input_data], columns=col_names)
-        score = pickled_model.predict(df) * 10
-        data.append({"location_id":loc, "busyness_score":str(score[0])})
+        input_data["DOLocationID"] = loc
+        df = pd.DataFrame(input_data, index=[0])
+        score = pickled_model.predict(df)
+        data.append({"location_id":loc, "busyness_score":score[0]})
+    
+    normalised_data = normalise_and_format(data) 
 
-    return data
+    return normalised_data
 
 def generate_model_input(date):
+    print(date)
     hour = date.hour
     dayOfMonth = date.day
     month = date.month
@@ -31,24 +34,24 @@ def generate_model_input(date):
 
     match day:
         case 1:
-            input_data["DayOfWeek0"] = 1
+            input_data["DayOfWeek_0"] = 1
         case 2:
-            input_data["DayOfWeek1"] = 1
+            input_data["DayOfWeek_1"] = 1
         case 3:
-            input_data["DayOfWeek2"] = 1
+            input_data["DayOfWeek_2"] = 1
         case 4: 
-            input_data["DayOfWeek3"] = 1
+            input_data["DayOfWeek_3"] = 1
         case 5:
-            input_data["DayOfWeek4"] = 1
+            input_data["DayOfWeek_4"] = 1
         case 6:
-            input_data["DayOfWeek5"] = 1
+            input_data["DayOfWeek_5"] = 1
         case 7:
-            input_data["DayOfWeek6"] = 1
+            input_data["DayOfWeek_6"] = 1
 
     if hour >= 0 and hour < 6:
         input_data["TimeOfDay_Night"] = 1
     elif hour < 12:
-        input_data["TimeOfDay_Morining"] = 1
+        input_data["TimeOfDay_Morning"] = 1
     elif hour < 17:
         input_data["TimeOfDay_Afternoon"] = 1
     elif hour < 22:
@@ -58,4 +61,18 @@ def generate_model_input(date):
 
     return input_data
 
+def normalise_and_format(data):
+    #get min and max for normalisation formula:
+    min, max = 0, 0
+    for item in data:
+        if item["busyness_score"] < min:
+            min = item["busyness_score"]
+        elif item["busyness_score"] > max:
+            max = item["busyness_score"]
 
+    #Normalise and format:
+    for item in data:
+        item["busyness_score"] = str((item["busyness_score"] - min) / (max - min))
+
+    return data
+    
