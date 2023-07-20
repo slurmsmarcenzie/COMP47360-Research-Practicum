25ad1c6e2a19410e2089292b88e27fa91f3c0549
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-20 16:43:12 +0100

    Remove redundant list_metric controller

--- machine learning/controllers/meta.py
+++ machine learning/controllers/meta.py
@@ -1,5 +1,4 @@
 from models.event import Event
-from models.metric import Metric
 from sqlalchemy import exc
 from flask import abort
 from logging_flask.logger import general_logger
@@ -27,31 +26,6 @@ def list_events():
             data.append(event.__dict__)
 
     general_logger.info("Event data is OK. Sending")
-    return data, 200
-
-# Query Database and get a list of Metric dictionaries
-# "_sa_instance_state" is removed as only field names are needed
-def list_metrics():
-    metrics = []
-    data = []
-
-    # Handle Database error:
-    try:
-        metrics = Metric.query.all()
-    except exc.SQLAlchemyError as er:
-        general_logger.error("Issue retreiving from database: {error}".format(error=er.orig))
-        raise abort(500, "There was an error retrieving metrics from the Database")
-
-    # Ensure data is in correct format:
-    if len(metrics) > 0:    
-        for metric in metrics:
-            if not isinstance(metric, Metric):
-                continue
-            if "_sa_instance_state" in metric.__dict__:
-                metric.__dict__.pop("_sa_instance_state")
-            data.append(metric.__dict__)
-
-    general_logger.info("Metric data is OK. Sending")
     return data, 200
 
     
