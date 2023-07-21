a57c12f3035d762b8b9c32f14b21bd8f4d7a39a9
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-21 09:47:07 +0100

    Refactor

--- machine learning/app.py
+++ machine learning/app.py
@@ -1,33 +1,37 @@
-from flask import Flask, request, make_response 
-from extensions.database import db
-from routes.blueprint import info, predict, base
-from logging_flask.logger import http_logger
-from extensions.limiter import limiter
-from controllers.base import login_manager, bcrypt
-import os
-from dotenv import load_dotenv
+from flask import Flask, request
 from flask_wtf.csrf import CSRFProtect
+from extensions.database import db
+from extensions.limiter import limiter
+from routes.blueprint import info, predict, base
+from controllers.base import login_manager, bcrypt
+from logging_flask.logger import http_logger
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+db.init_app(app)
 limiter.init_app(app)
+login_manager.init_app(app)
 bcrypt.init_app(app)
 csrf = CSRFProtect(app)
-load_dotenv()
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLDB")
 app.config["SECRET_KEY"] = os.getenv("SECRETKEY")
 app.config["RECAPTCHA_PUBLIC_KEY"] = os.getenv("RECAPTCHA_PUBLIC")
 app.config["RECAPTCHA_PRIVATE_KEY"] = os.getenv("RECAPTCHA_PRIVATE")
 # app.config["SESSION_COOKIE_SAMESITE"] = "strict"
 # app.config["SESSION_COOKIE_HTTPONLY"] = True
-db.init_app(app)
+
 app.register_blueprint(info)
 app.register_blueprint(predict)
 app.register_blueprint(base)
-login_manager.init_app(app)
 
 @app.after_request
 def set_headers(response):
-    ##Note: these may need to be set at the web server level, not here
+    ## Note: these may need to be set at the web server level, not here
+    ## Note: commented out for now. Headers were causing issues
     #response.headers['Content-Security-Policy'] = "default-src 'self'"
     # response.headers['X-Content-Type-Options'] = 'nosniff'
     # response.headers['X-Frame-Options'] = 'SAMEORIGIN'
