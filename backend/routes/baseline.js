4de7ff4a7fd8f266f0f14595fe81f6cb19cc2d8a
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-21 11:20:49 +0100

    Commenting

--- backend/routes/baseline.js
+++ backend/routes/baseline.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {queryBaseline, queryEventBaseline} = require("../controllers/baseline");
 
+// Router for 'app/v1/baseline/$date/$event' and 'app/v1/baseline'
 router.route("/:date/:event").get(queryEventBaseline)
 router.route("").get(queryBaseline);
 
--- backend/routes/meta.js
+++ backend/routes/meta.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {getEvents} = require("../controllers/meta");
 
+// Router for 'app/v1/meta/events'
 router.route("/events").get(getEvents);
 
 module.exports = router;
