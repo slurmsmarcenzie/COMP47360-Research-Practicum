f53020ddb2da813d48379b656ebe4caebe77f551
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-21 10:16:08 +0100

    Commenting

--- machine learning/extensions/limiter.py
+++ machine learning/extensions/limiter.py
@@ -1,4 +1,5 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+# Initialize limiter. Set request limit to 200 per day
 limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day"])
