c7a482bcd9f18f4b7dbe3f50391b40a99548582c
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-21 11:17:42 +0100

    Remove metrics route

--- backend/routes/meta.js
+++ backend/routes/meta.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {getEvents, getMetrics} = require("../controllers/meta");
-
+const {getEvents} = require("../controllers/meta");
 
 router.route("/events").get(getEvents);
-router.route("/metrics").get(getMetrics);
 
 module.exports = router;
