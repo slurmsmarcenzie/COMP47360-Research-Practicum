dc81e18b9747d9444cdd6edfb26b3bacca35b637
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-21 11:14:00 +0100

    Refactor and update routes Removed build folder

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,44 @@
 const express = require('express')
 const app = express()
-const cors = require("cors");
 const meta = require("./routes/meta")
 const prediction = require("./routes/prediction")
-const path = require("path")
 const baseline = require("./routes/baseline")
-const port = process.env.PORT || 5000
 const http_logger = require("./middleware/http_logger");
 const rateLimiter = require("./middleware/rate_limiter")
 const helmet = require('helmet')
+const cors = require("cors");
+const path = require("path")
 require("dotenv").config();
-
 
 
 //Middleware:
 app.use(cors());
 app.use(express.json())
+app.use(rateLimiter)
+app.disable('x-powered-by')
+// Turn on later:
 // app.use(helmet.frameguard())
 // app.use(helmet.noSniff());
-//app.use(helmet.contentSecurityPolicy())
-app.use(rateLimiter)
-//Below is used to serve a build version of the React frontend application:
+// app.use(helmet.contentSecurityPolicy())
+
+// Serve React App Build:
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/", function(req, res) {res.sendFile(path.join(__dirname, "build", "index.html"))})
 
 //Routes:
-app.use("/api/meta", meta, http_logger)
-app.use("/api/predict", prediction, http_logger)
-app.use("/api/baseline", baseline, http_logger)
+app.use("/app/v1/meta", meta, http_logger)
+app.use("/app/v1/prediction", prediction, http_logger)
+app.use("/app/v1/baseline", baseline, http_logger)
+// In future: potentially more routes for filtering/sorting results
 
-// app.get('/', (req, res, next) => {
-//   res.status(200).send("Home Page. I will serve React App later :)")
-//   next()
-// }, http_logger);
-
+// Handle bad routes
 app.get('*', (req, res, next) => {
   res.status(404).send("Unknown route. Please check the URL entered")
   next()
 }, http_logger);
 
 
-app.disable('x-powered-by')
+const port = process.env.PORT || 5000
 //START SERVER
 app.listen(port, () => {
     console.log('server is listening on port', port)
