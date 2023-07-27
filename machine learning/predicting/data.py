0653e2d304bcc9263c4a85a505f39b3963865725
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-07-27 16:53:12 +0100

    remove col_names_event

--- machine learning/predicting/data.py
+++ machine learning/predicting/data.py
@@ -1,5 +1,3 @@
 col_names_general = ['DOLocationID', 'Hour', 'DayOfMonth', 'Month', 'Weekend', 'TimeOfDay_Afternoon', 'TimeOfDay_Evening', 'TimeOfDay_Morning', 'TimeOfDay_Night', 'DayOfWeek_0', 'DayOfWeek_1', 'DayOfWeek_2', 'DayOfWeek_3', 'DayOfWeek_4', 'DayOfWeek_5', 'DayOfWeek_6']
 
-col_names_event = ['Event_ID_1', "Event_ID_2", "Event_ID_3", "Event_ID_4", "Event_ID_5", "Event_ID_6", "Event_ID_7", "Event_ID_8", "DOLocationID", "time", "TimeOfDay_Evening", "TimeOfDay_Morning", "TimeOfDay_Night", "num_dropoffs", "num_dropoffs_event"]
-
 location_ids = [236, 42, 166, 68, 163, 87, 152, 141, 229, 90, 113, 79, 140, 151, 107, 263, 43, 24, 233, 238, 237, 249, 186, 262, 74, 4, 45, 48, 142, 170, 137, 261, 246, 41, 239, 148, 243, 153, 231, 114, 211, 164, 144, 13, 161, 125, 50, 162, 234, 202, 224, 244, 158, 232, 88, 75, 127, 143, 116, 100, 209, 120, 230, 12, 194, 128, 105]
