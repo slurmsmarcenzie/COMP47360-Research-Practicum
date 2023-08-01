936b4f874535d27632f5080de39049ebe11a038f
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-08-01 17:53:27 +0100

    Minor update

--- backend/controllers/validations/validator.js
+++ backend/controllers/validations/validator.js
@@ -3,7 +3,7 @@ const generalLogger = require("../../logging/generalLogger")(module)
 /** Enumeration of Location information */
 const LOCATIONS = {
   LocNumMin:35,
-  LocNumExpected:66,
+  LocNumExpected:67,
   LocKeys: ["236", "42", "166", "68", "163", "87", "152", "141", "229", "90", "113", "79", "140", "151", "107", "263", "43", "24", "233", "238", "237", "249", "186", "262", "74", "4", "45", "48", "142", "170", "137", "261", "246", "41", "239", "148", "243", "153", "231", "114", "211", "164", "144", "13", "161", "125", "50", "162", "234", "202", "224", "244", "158", "232", "88", "752", "127", "143", "116", "100", "209", "120", "230", "12", "194", "128", "105"]
 }
 
@@ -15,8 +15,9 @@ const TIMES = {
 
 /** Enumeration of the valid Busyness Score range */
 const RANGE= {
-  min:-1,
-  max:1
+  // expected range is -1 to +1. However 2 is better to tolerate conversion changes
+  min:-2,
+  max:2
 }
 
 /**
@@ -133,7 +134,7 @@ const validate_response_hour = (response_data) => {
     }
     // Check that busyness scores are in the range -1 to +1
     if (score < RANGE.min || score > RANGE.max){
-      generalLogger.error(`response contains busynes score outside of range 0 - 1: ${score}`)
+      generalLogger.error(`response contains busyness score outside of range 0 - 1: ${score}`)
       return false
     }
   }
