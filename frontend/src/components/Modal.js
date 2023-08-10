e061fd1b3b673628794d0b7c0831b84bd2d36997
Maya Clinton <mayak.clinton@gmail.com>
2023-08-10 13:09:27 +0100

    scrollbar

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../App.css';
-import afterParty from '../images/after-party-logo-white.webp';
+import afterParty from '../images/EINYCwhite.png';
 
 
 export default function Modal({ onClose }) {
