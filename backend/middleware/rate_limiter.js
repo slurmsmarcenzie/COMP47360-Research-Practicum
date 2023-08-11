3cbd79ae3a526240e9f09d97314e109bf141381c
slurmsmarcenzie <98986933+slurmsmarcenzie@users.noreply.github.com>
2023-08-11 12:19:50 +0100

    Update rate_limiter.js
    change rate limiter so isn't as strict

--- backend/middleware/rate_limiter.js
+++ backend/middleware/rate_limiter.js
@@ -1,13 +1,25 @@
+//rate limitter 
 const rateLimit = require("express-rate-limit")
 
-/** 
- * Middleware function to limit the amount of requests from a given IP
- */
+
+
+// Below is commented out, it was just to allow me to keep checking with my ip without being blocked, put your ip in IPHERE if wish
+// const allowedIP = "IPHERE"; 
+
+// Custom function to skip rate limiting for your IP
+// const skipRateLimitForIP = (req) => {
+//   return req.ip === allowedIP;
+// };
+
+
+
+// Middleware function to limit the amount of requests from a given IP
 const rateLimiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 min
-	max: 30, // Limit to 30 requests per 1 min
-	standardHeaders: true,
-	legacyHeaders: false
+        windowMs: 1 * 60 * 1000, // 1 min
+        max: 100, // Limit to 30 requests per 1 min
+        skip: skipRateLimitForIP,
+        standardHeaders: true,
+        legacyHeaders: false
 })
 
 module.exports = rateLimiter;
